2eb70dbfae004b47be972cb5016c13b9d6c66084
Kabir <kabir_oberoi@hotmail.com>
2020-05-02 04:43:16 -0400

    Updated bash file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 export AUTH0_DOMAIN=kprod.auth0.com
 export ALGORITHMS=['RS256']
 export API_AUDIENCE=castingAgency
-export DATABASE_URL=postgres://znjwtsmkzfozca:59bcec5b00859bcf407466a555657eb712b260d8d06b512f9420532a13043d2c@ec2-34-234-228-127.compute-1.amazonaws.com:5432/d2mulmshfgth62
+export DATABASE_URL=postgres://pevpyesvwujver:a89d03f5df4e3fcda5cf1c521a31a8bf4e5495c77cb81afaf8c66288d55456d5@ec2-50-17-21-170.compute-1.amazonaws.com:5432/dbf8u5vfthr0ot
 export AUTH0_LOGIN=https://kprod.auth0.com/authorize?audience=castingAgency&response_type=token&client_id=WwlsQzNVr9q37zz71Crr6zTofItGU9ZI&redirect_uri=http://localhost:5432/login-results
 export ENV=prod
